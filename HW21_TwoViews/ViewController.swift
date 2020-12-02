3eb304f01b8974718129b86f4a4ea4aded10d01f
Andrey Terechshenkov <aterechshenkov@gmail.com>
2020-12-02 13:20:10 +0600

    Initial Commit

---
+++ HW21_TwoViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW21_TwoViews
+//
+//  Created by Terechshenkov Andrey on 12/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
